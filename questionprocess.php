57ddaae39f450fb69ad897bfde67342217384c0f
antonycs <antonycs007@gmail.com>
2017-05-04 20:02:24 +0530

    some validations

--- questionprocess.php
+++ questionprocess.php
@@ -3,23 +3,29 @@ require_once("connection.php");
 if ((isset($_POST['qst']))&&(isset($_POST['ans']))) {
   session_start();
   if (isset($_SESSION['count'])) {
-    if ($_SESSION['count']<3) {
+    if ($_SESSION['count']<=1) {
 
       $w=$_SESSION['count'];
         $_SESSION['count']=$w+1;
       $i=$_SESSION['count'];
       $_SESSION['qst'][$i]=$_POST['qst'];
       $_SESSION['ans'][$i]=$_POST['ans'];
-      $a = "success";
+      if ($_SESSION['count']<=1){
+        $a = "Success, add one more ?";
+      }else {
+$a = "Limit reached!, Register now.";
+      }
+
+
     } else {
-      $a = "Limit reached!";
+      $a = "Limit reached!, Register now.";
     }
   } else {
     $_SESSION['count']=0;
     $i=$_SESSION['count'];
     $_SESSION['qst'][$i]=$_POST['qst'];
     $_SESSION['ans'][$i]=$_POST['ans'];
-    $a = "success1";
+    $a = "Success, add two more ?s";
   }
 } else {
   $a = "Something went wrong!";
