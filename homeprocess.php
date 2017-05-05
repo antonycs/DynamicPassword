0a39761767f8f46ddb93d85a7637f403549adb36
antonycs <antonycs007@gmail.com>
2017-05-05 19:03:22 +0530

    solved redirection issue

--- homeprocess.php
+++ homeprocess.php
@@ -1,18 +1,22 @@
 <?php
 require_once("connection.php");
 session_start();
+$a = array();
 if (isset($_POST['ter'])) {
 if(!isset($_SESSION['user_id'])){
-  header("location:index.html");
-}
-  $a = array();
+  $a['user_name']="goback";
+  // header("location:index.html");
+}else {
+
 $u = $_SESSION['user_id'];
 $q = mysqli_query($con,"SELECT * FROM registeration WHERE user_id=$u");
 if(mysqli_num_rows($q)>0){
   $values = mysqli_fetch_assoc($q);
   $a['a']=$values;
   $a['user_name']=$_SESSION['uname'];
+
+}
+}
+}
   echo json_encode($a);
-}
-}
  ?>
